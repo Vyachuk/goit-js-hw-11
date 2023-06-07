a288b33b48a5d13916a15fbdd685ef4b305743d7
Vyachuk <Slavkoka045@gmail.com>
2023-06-07 12:24:19 +0300

    update fetchApi File

--- src/js/fetchApi.js
+++ src/js/fetchApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const API_KEY = '36986465-56bab1af629ac84504d809b09';
 
-export async function fetchApi({name, page }) {
+export function fetchApi({name, page }) {
     const searchParams = new URLSearchParams({
         key: API_KEY,
         q: name,
@@ -13,8 +13,7 @@ export async function fetchApi({name, page }) {
         per_page: 40,
     });
     try {
-        const response = await axios.get(`https://pixabay.com/api/?${searchParams}`)
-        return response;
+        return axios.get(`https://pixabay.com/api/?${searchParams}`)
     } catch (error) {
         console.log(error);
     }
