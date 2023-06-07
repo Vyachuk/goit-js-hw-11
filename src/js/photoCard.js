8da0e8f20e7765649d6871f80261be7440ee3187
Vyachuk <Slavkoka045@gmail.com>
2023-06-07 16:28:36 +0300

    add css

--- src/js/photoCard.js
+++ src/js/photoCard.js
@@ -4,16 +4,16 @@ export const onItemHtml = item => {
             <img height=170 src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
             <div class="info">
                 <p class="info-item">
-                    <b>Likes ${item.likes}</b>
+                    Likes <br> ${item.likes}
                 </p>
                 <p class="info-item">
-                    <b>Views ${item.views}</b>
+                    Views <br> ${item.views}
                 </p>
                 <p class="info-item">
-                    <b>Comments ${item.comments}</b>
+                    Comments <br> ${item.comments}
                 </p>
                 <p class="info-item">
-                    <b>Downloads ${item.downloads}</b>
+                    Downloads <br> ${item.downloads}
                 </p>
             </div>
         </div>`
